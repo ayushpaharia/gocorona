461214f845862f01537d8be011d0e9936c836874
ayushpaharia <ayush.paharia.18@gmail.com>
2020-04-03 19:11:23 +0530

    ui changes

--- js/data.js
+++ js/data.js
@@ -9,14 +9,14 @@ const getData = async () => {
     .then(data => {
       totalest.innerHTML += `
                 <div class="small">
-                    <div class="title">
+                    <div class="title" style="font-size: 4vw;font-weight:800">
                         Stats
                     </div>
                     <div class="est" style="display: flex; flex-direction: column;">
-                      <span><span style="color: #337ab7">Confirmed</span>: ${data.statewise[0].confirmed}</span>
-                      <span><span style="color: #f0ad4e">Active</span>: ${data.statewise[0].active}</span>
-                      <span><span style="color: #5cb85c">Recovered</span>: ${data.statewise[0].recovered}</span>
-                      <span><span style="color: #d9534f">Deaths</span>: ${data.statewise[0].deaths}</span>
+                      <span><span style="font-weight:700;font-size:2.5vw;color: #337ab7">Confirmed</span>: ${data.statewise[0].confirmed}</span>
+                      <span><span style="font-weight:700;font-size:2.5vw;color: #f0ad4e">Active</span>: ${data.statewise[0].active}</span>
+                      <span><span style="font-weight:700;font-size:2.5vw;color: #5cb85c">Recovered</span>: ${data.statewise[0].recovered}</span>
+                      <span><span style="font-weight:700;font-size:2.5vw;color: #d9534f">Deaths</span>: ${data.statewise[0].deaths}</span>
                     </div>
                 </div>`;
 
